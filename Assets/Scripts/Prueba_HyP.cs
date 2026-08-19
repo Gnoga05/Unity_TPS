cab43d33bf7a22bbef770a99538cbfd4076f860e
GabrielNogarotto <gabrielnogarotto2@gmail.com>
2026-08-19 20:23:04 -0300

    Unity_Proyect

---
+++ Assets/Scripts/Prueba_HyP.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class Prueba_HyP : MonoBehaviour
+{
+    string Nombre = "GABRIEL";
+
+    int Edad = 20;
+
+    int Fecha = 11102005;
+
+
+ void Start()
+
+    {
+
+    Debug.Log(Nombre + "" + Edad + "" + Fecha);
+
+    }   
+
+void Update()
+
+    {
+
+    }
+
+}
+
+
+   
